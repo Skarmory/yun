feed02663b937022ee26589be19049d9d9ac0ab5
Song Taezhou <song.taezhou@gmail.com>
2017-10-09 10:54:39 +0100

    Added extern races array to race.h
    Added a macro for nicer defining of races in the array

--- include/race.h
+++ include/race.h
@@ -3,15 +3,15 @@
 
 #include "faction.h"
 
-// Player race defines
+// Race masks 
 #define RA_HUMAN    0x00000001
 #define RA_DWARF    0x00000002
-#define RA_NELF     0x00000004 
-#define RA_GNOME    0x00000008 
-#define RA_ORC      0x00000010 
+#define RA_NELF     0x00000004
+#define RA_GNOME    0x00000008
+#define RA_ORC      0x00000010
 #define RA_FORSAKEN 0x00000020
 #define RA_TAUREN   0x00000040
-#define RA_TROLL    0x00000080 
+#define RA_TROLL    0x00000080
 
 struct Race
 {
@@ -23,5 +23,7 @@ struct Race
     short selfmask;
     short faction;
 };
+
+extern struct Race races[];
 
 #endif
--- src/race.c
+++ src/race.c
@@ -1,45 +1,18 @@
 #include "race.h"
 
-const struct Race races[] =
+#define RACE(hk, noun, adj, home, lead, smask, fac) \
+    {\
+      hk, noun, adj, home, lead, smask, fac  \
+    }\
+
+struct Race races[] =
 {
-    {
-        'h', "human", "human", "Stormwind", "Highlord Fordragon",
-        RA_HUMAN,
-        FA_ALLIANCE,
-    },
-    {
-        'd', "dwarf", "dwarven", "Ironforge", "King Bronzebeard",
-        RA_DWARF,
-        FA_ALLIANCE,
-    },
-    {
-        'e', "night elf", "night elven", "Darnassus", "High Priestess Whisperwind",
-        RA_NELF,
-        FA_ALLIANCE,
-    },
-    {
-        'g', "gnome", "gnomish", "Ironforge", "High Tinker Mekkatorque",
-        RA_GNOME,
-        FA_ALLIANCE,
-    },
-    {
-        'o', "orc", "orcish", "Orgrimmar", "Warchief Thrall",
-        RA_ORC,
-        FA_HORDE,
-    },
-    {
-        'f', "forsaken", "forsaken", "The Undercity", "Banshee Queen Windrunner",
-        RA_FORSAKEN,
-        FA_HORDE,
-    },
-    {
-        't', "tauren", "tauren", "Thunder Bluff", "Chieftain Bloodhoof",
-        RA_TAUREN,
-        FA_HORDE,
-    },
-    {
-        'l', "troll", "troll", "Orgrimmar", "Darkspear Leader Vol'jin",
-        RA_TROLL,
-        FA_HORDE,
-    }
+    RACE('h', "human", "human", "Stormwind", "Highlord Fordragon", RA_HUMAN, FA_ALLIANCE),
+    RACE('d', "dwarf", "dwarven", "Ironforge", "King Bronzebeard", RA_DWARF, FA_ALLIANCE),
+    RACE('e', "night elf", "night elven", "Darnassus", "High Priestess Whisperwind", RA_NELF, FA_ALLIANCE),
+    RACE('g', "gnome", "gnomish", "Ironforge", "High Tinker Mekkatorque", RA_GNOME, FA_ALLIANCE),
+    RACE('o', "orc", "orcish", "Orgrimmar", "Warchief Thrall", RA_ORC, FA_HORDE),
+    RACE('f', "forsaken", "forsaken", "The Undercity", "Banshee Queen Windrunner", RA_FORSAKEN, FA_HORDE),
+    RACE('t', "tauren", "tauren", "Thunder Bluff", "Chieftain Bloodhoof", RA_TAUREN, FA_HORDE),
+    RACE('l', "troll", "troll", "Orgrimmar", "Darkspear Leader Vol'jin", RA_TROLL, FA_HORDE)
 };
