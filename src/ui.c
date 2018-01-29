249074842aca70c272bbe83ff4fb71c2d99ccec3
Song Taezhou <song.taezhou@gmail.com>
2018-01-29 13:27:18 +0000

    Updated UI to display percentages

--- src/ui.c
+++ src/ui.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ncurses.h>
+#include "log.h"
 
 #define STATUS_X 0
 #define STATUS_Y 45
@@ -24,12 +25,12 @@ void display_char_info_screen(void)
     clear();
 
     attron(A_BOLD);
-    mvprintw(0, 0, "Strength             Agility              Intelligence       Spirit                 Stamina");
+    mvprintw(0, 0, "Strength              Agility               Intelligence         Spirit                  Stamina");
     attroff(A_BOLD);
-    mvprintw(1, 0, "Attack Power: %d      Attack Power: %d      Mana:        %d     HP Regen:     %d/turn   Hit Points:        %d", PSTAT(strength, attack_power), PSTAT(agility, attack_power), PSTAT(intelligence, max_mana), PSTAT(spirit, health_regen), PSTAT(stamina, max_health));
-    mvprintw(2, 0, "Armour Pen:   %2.2f   Crit Chance:  %2.2f   Spell Pen:   %d     Mana Regen    %d/turn   Block Chance:      %2.2f", PSTAT(strength, armour_pen), PSTAT(agility, crit_chance), PSTAT(intelligence, spell_pen), PSTAT(spirit, mana_regen), PSTAT(stamina, block_chance));
-    mvprintw(3, 0, "Parry Chance: %2.2f   Dodge Chance: %2.2f   Spell Power: %d     Spell Power:  %d        Crit Block Chance: %2.2f", PSTAT(strength, parry_chance), PSTAT(agility, dodge_chance), PSTAT(intelligence, spell_power), PSTAT(spirit, spell_power), PSTAT(stamina, crit_block_chance));
-    mvprintw(4, 0, "                                          Spell Crit:  %2.2f  Spell Resist: %2.2f     Block Amount:      %d", PSTAT(intelligence, spell_crit_chance), PSTAT(spirit, resist), PSTAT(stamina, block_amount) );
+    mvprintw(1, 0, "Attack Power: %d       Attack Power: %d       Mana:        %d       HP Regen:     %d/turn    Hit Points:        %d", PSTAT(strength, attack_power), PSTAT(agility, attack_power), PSTAT(intelligence, max_mana), PSTAT(spirit, health_regen), PSTAT(stamina, max_health));
+    mvprintw(2, 0, "Armour Pen:   %2.2f%%   Crit Chance:  %2.2f%%   Spell Pen:   %d       Mana Regen    %d/turn    Block Chance:      %2.2f%%", PSTAT(strength, armour_pen) * 100.f, PSTAT(agility, crit_chance) * 100.f, PSTAT(intelligence, spell_pen), PSTAT(spirit, mana_regen), PSTAT(stamina, block_chance) * 100.f);
+    mvprintw(3, 0, "Parry Chance: %2.2f%%   Dodge Chance: %2.2f%%   Spell Power: %d       Spell Power:  %d         Crit Block Chance: %2.2f%%", PSTAT(strength, parry_chance) * 100.f, PSTAT(agility, dodge_chance) * 100.f, PSTAT(intelligence, spell_power), PSTAT(spirit, spell_power), PSTAT(stamina, crit_block_chance) * 100.f);
+    mvprintw(4, 0, "                                            Spell Crit:  %2.2f%%   Spell Resist: %2.2f%%     Block Amount:      %d", PSTAT(intelligence, spell_crit_chance) * 100.f, PSTAT(spirit, resist) * 100.f, PSTAT(stamina, block_amount) );
 
     getch();
 
