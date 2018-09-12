08692836c4bae9509dbc031c7c6edde2dd8a6c2b
Song Taezhou <song.taezhou@gmail.com>
2018-09-12 13:58:39 +0100

    Updated UI drawing
    restricted parameter warning now taken care of, as well as overflow.
    
    Updated format strings for character display to make sure that only a
    fixed amount of space will be used and to align everything easier

--- src/ui.c
+++ src/ui.c
@@ -17,9 +17,11 @@
 void display_char_status(void)
 {
     char tmp[STATUS_W];
-    sprintf(tmp, "%s\t", you->name);
-    sprintf(tmp, "%sHP: %d/%d  MP: %d/%d\t", tmp, HP(you->mon), MAXHP(you->mon), MANA(you->mon), MAXMANA(you->mon));
-    sprintf(tmp, "%sStr:%d Agi:%d Int:%d Spi:%d Sta:%d", tmp, PSTAT(strength, strength), PSTAT(agility, agility), PSTAT(intelligence, intelligence), PSTAT(spirit, spirit), PSTAT(stamina, stamina));
+    sprintf(
+        tmp, "%s\tHP: %d/%d  MP: %d/%d\tStr:%d Agi:%d Int:%d Spi:%d Sta:%d", you->name,
+        HP(you->mon), MAXHP(you->mon), MANA(you->mon), MAXMANA(you->mon),
+        PSTAT(strength, strength), PSTAT(agility, agility), PSTAT(intelligence, intelligence), PSTAT(spirit, spirit), PSTAT(stamina, stamina)
+    );
 
     mvprintw(STATUS_Y, STATUS_X, tmp);
 }
@@ -35,11 +37,11 @@ void display_char_info_screen(void)
     mvprintw_xy(1, 1, "Class: %s", you->cls->name);
     mvprintw_xy(1, 2, "Race:  %s", you->race->noun);
 
-    mvprintwa(1, 5, A_BOLD, "Strength              Agility               Intelligence         Spirit                  Stamina");
-    mvprintw_xy(1, 6, "Attack Power: %d       Attack Power: %d       Mana:        %d       HP Regen:     %d/turn    Hit Points:        %d", PSTAT(strength, attack_power), PSTAT(agility, attack_power), PSTAT(intelligence, max_mana), PSTAT(spirit, health_regen), PSTAT(stamina, max_health));
-    mvprintw_xy(1, 7, "Armour Pen:   %2.2f    Crit Chance:  %2.2f    Spell Pen:   %d       Mana Regen    %d/turn    Block Chance:      %2.2f", PSTAT(strength, armour_pen) * 100.f, PSTAT(agility, crit_chance) * 100.f, PSTAT(intelligence, spell_pen), PSTAT(spirit, mana_regen), PSTAT(stamina, block_chance) * 100.f);
-    mvprintw_xy(1, 8, "Parry Chance: %2.2f    Dodge Chance: %2.2f    Spell Power: %d       Spell Power:  %d         Crit Block Chance: %2.2f", PSTAT(strength, parry_chance) * 100.f, PSTAT(agility, dodge_chance) * 100.f, PSTAT(intelligence, spell_power), PSTAT(spirit, spell_power), PSTAT(stamina, crit_block_chance) * 100.f);
-    mvprintw_xy(1, 9, "                                            Spell Crit:  %2.2f    Spell Resist: %2.2f      Block Amount:      %d", PSTAT(intelligence, spell_crit_chance) * 100.f, PSTAT(spirit, resist) * 100.f, PSTAT(stamina, block_amount) );
+    mvprintwa(1, 5, A_BOLD, "Strength       Agility        Intelligence   Spirit         Stamina");
+    mvprintw_xy(1, 6, "AP:     %05d  AP:     %05d  Mana:   %05d  HRegen: %03d/t  HP:         %05d", PSTAT(strength, attack_power), PSTAT(agility, attack_power), PSTAT(intelligence, max_mana), PSTAT(spirit, health_regen), PSTAT(stamina, max_health));
+    mvprintw_xy(1, 7, "ArmPen: %05.2f  Crit%:  %05.2f  SpePen: %05d  MRegen: %03d/t  Block%:     %05.2f", PSTAT(strength, armour_pen) * 100.f, PSTAT(agility, crit_chance) * 100.f, PSTAT(intelligence, spell_pen), PSTAT(spirit, mana_regen), PSTAT(stamina, block_chance) * 100.f);
+    mvprintw_xy(1, 8, "Parry%: %05.2f  Dodge%: %05.2f  SP:     %05d  SP:     %05d  CritBlock%: %05.2f", PSTAT(strength, parry_chance) * 100.f, PSTAT(agility, dodge_chance) * 100.f, PSTAT(intelligence, spell_power), PSTAT(spirit, spell_power), PSTAT(stamina, crit_block_chance) * 100.f);
+    mvprintw_xy(1, 9, "                              SCrit%: %05.2f  SRes%:  %05.2f  Block:      %05d", PSTAT(intelligence, spell_crit_chance) * 100.f, PSTAT(spirit, resist) * 100.f, PSTAT(stamina, block_amount) );
 
     getch();
 
