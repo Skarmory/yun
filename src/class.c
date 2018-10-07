a6ff8297d99c833098cb622c7c2801e7506fb475
Song Taezhou <song.taezhou@gmail.com>
2018-10-07 11:49:04 +0100

    Updated class stat ups and race base stats
    Took the actual values from WoW that each race has and how many extra
    stats each class gives

--- src/class.c
+++ src/class.c
@@ -20,31 +20,31 @@
 struct Class classes[] = 
 {
     CLS("druid", "druids", 'd', RA_NELF | RA_TAUREN, CLS_DRUID, 
-        STATUPS(2, 2, 2, 1, 1), STATSCA(0.5f, 0.5f, 0.75f, 0.5f, 0.75f)),
+        STATUPS(1, 0, 2, 2, 0), STATSCA(0.5f, 0.5f, 0.75f, 0.5f, 0.75f)),
 
     CLS("hunter", "hunters", 'h', RA_DWARF | RA_NELF | RA_ORC | RA_TAUREN | RA_TROLL, CLS_HUNTER, 
-        STATUPS(1, 3, 1, 1, 2), STATSCA(0.3f, 1.0f, 0.7f, 0.5f, 0.5f)),
+        STATUPS(0, 3, 0, 1, 1), STATSCA(0.3f, 1.0f, 0.7f, 0.5f, 0.5f)),
 
     CLS("mage", "magi", 'm', RA_HUMAN | RA_DWARF | RA_GNOME | RA_FORSAKEN | RA_TROLL, CLS_MAGE, 
-        STATUPS(1, 1, 3, 2, 1), STATSCA(0.1f, 0.1f, 2.0f, 0.5f, 0.2f)),
+        STATUPS(0, 0, 3, 2, 0), STATSCA(0.1f, 0.1f, 2.0f, 0.5f, 0.2f)),
 
     CLS("rogue", "rogues", 'r', RA_HUMAN | RA_DWARF | RA_NELF | RA_GNOME | RA_ORC | RA_FORSAKEN | RA_TROLL, CLS_ROGUE, 
-        STATUPS(1, 3, 1, 1, 2), STATSCA(0.5f, 1.5f, 0.35f, 0.35f, 0.3f)),
+        STATUPS(1, 3, 0, 0, 1), STATSCA(0.5f, 1.5f, 0.35f, 0.35f, 0.3f)),
 
     CLS("paladin", "paladins", 'p', RA_HUMAN | RA_DWARF, CLS_PALADIN, 
-        STATUPS(2, 1, 1, 1, 3), STATSCA(0.75f, 0.35f, 0.75f, 0.45f, 0.75f)),
+        STATUPS(2, 0, 0, 1, 2), STATSCA(0.75f, 0.35f, 0.75f, 0.45f, 0.75f)),
 
     CLS("priest", "priests", 'i', RA_HUMAN | RA_DWARF | RA_NELF | RA_FORSAKEN | RA_TROLL, CLS_PRIEST, 
-        STATUPS(1, 1, 2, 3, 1), STATSCA(0.15f, 0.25f, 0.65f, 1.5f, 0.5f)),
+        STATUPS(0, 0, 2, 3, 0), STATSCA(0.15f, 0.25f, 0.65f, 1.5f, 0.5f)),
 
     CLS("shaman", "shamans", 's', RA_ORC | RA_TAUREN | RA_TROLL, CLS_SHAMAN, 
-        STATUPS(2, 1, 3, 1, 1), STATSCA(0.75f, 0.75f, 0.75f, 0.35f, 0.4f)),
+        STATUPS(1, 0, 1, 2, 1), STATSCA(0.75f, 0.75f, 0.75f, 0.35f, 0.4f)),
 
     CLS("warlock", "warlocks", 'l', RA_HUMAN | RA_GNOME | RA_ORC | RA_FORSAKEN, CLS_WARLOCK, 
-        STATUPS(1, 1, 3, 1, 2), STATSCA(0.15f, 0.25f, 1.5f, 0.6f, 0.5f)),
+        STATUPS(0, 0, 2, 2, 1), STATSCA(0.15f, 0.25f, 1.5f, 0.6f, 0.5f)),
 
     CLS("warrior", "warriors", 'w', RA_HUMAN | RA_DWARF | RA_NELF | RA_GNOME | RA_ORC | RA_FORSAKEN | RA_TAUREN | RA_TROLL, CLS_WARRIOR, 
-        STATUPS(3, 1, 1, 1, 2), STATSCA(1.0f, 0.5f, 0.1f, 0.4f, 1.0f))
+        STATUPS(3, 0, 0, 0, 2), STATSCA(1.0f, 0.5f, 0.1f, 0.4f, 1.0f))
 };
 
 /**
