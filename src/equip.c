080262d532ec116b0eb18a0b21de3476731c76e1
Song Taezhou <song.taezhou@gmail.com>
2018-10-28 15:30:38 +0000

    Fixed bug caused by trying to free NULL weapons

--- src/equip.c
+++ src/equip.c
@@ -7,12 +7,17 @@
 struct Equipment* new_equipment(void)
 {
     struct Equipment* equipment = (struct Equipment*) malloc(sizeof(struct Equipment));
+    equipment->main_hand = NULL;
+    equipment->off_hand = NULL;
 
     return equipment; 
 }
 
 void free_equipment(struct Equipment* equipment)
 {
-    free_weapon(equipment->main_hand);
-    free_weapon(equipment->off_hand); 
+    if(equipment->main_hand)
+        free_weapon(equipment->main_hand);
+
+    if(equipment->off_hand)
+        free_weapon(equipment->off_hand);
 }
