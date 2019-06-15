877df58a316eebde4bf2106ad558d9eaf443661f
Song Taezhou <song.taezhou@gmail.com>
2019-06-15 22:26:34 +0100

    Added ArmourBase and removed static armour data

--- include/obj_armour.h
+++ include/obj_armour.h
@@ -6,31 +6,6 @@
 
 struct Object;
 
-#define A_UNARMOURED 0
-#define A_MAGISTERS_CROWN 1
-#define A_MAGISTERS_MANTLE 2
-#define A_MAGISTERS_ROBE 3
-#define A_MAGISTERS_GLOVES 4
-#define A_MAGISTERS_LEGGINGS 5
-#define A_MAGISTERS_BOOTS 6
-#define A_SHADOWCRAFT_CAP 7
-#define A_SHADOWCRAFT_SPAULDERS 8
-#define A_SHADOWCRAFT_TUNIC 9
-#define A_SHADOWCRAFT_GLOVES 10
-#define A_SHADOWCRAFT_PANTS 11
-#define A_SHADOWCRAFT_BOOTS 12
-#define A_BEASTSTALKERS_CAP 13
-#define A_BEASTSTALKERS_SPAULDERS 14
-#define A_BEASTSTALKERS_TUNIC 15
-#define A_BEASTSTALKERS_GLOVES 16
-#define A_BEASTSTALKERS_PANTS 17
-#define A_BEASTSTALKERS_BOOTS 18
-#define A_VALOR_HELM 19
-#define A_VALOR_SPAULDERS 20
-#define A_VALOR_BREASTPLATE 21
-#define A_VALOR_GAUNTLETS 22
-#define A_VALOR_LEGPLATES 23
-#define A_VALOR_BOOTS 24
 enum ArmourClass
 {
     ARMOUR_CLASS_CLOTH,
@@ -52,21 +27,18 @@ struct ArmourBase
 struct Armour
 {
     int armour_value;
-    enum EquipmentSlot slot;
-    byte strength;
-    byte agility;
-    byte intelligence;
-    byte spirit;
-    byte stamina;
 
-    struct Object* obj;
+    struct ArmourBase* base;
+    struct Object*     obj;
 };
 
-struct Armour* new_armour(int armour_type);
-void free_armour(struct Armour* armour);
+struct Armour* armour_new(struct ArmourBase* base);
+void armour_free(struct Armour* armour);
 
-extern struct Armour armours[];
-extern char* armour_names[];
-extern char* armour_descs[];
+struct ArmourBase* armour_base_lookup_by_name(const char* name);
+enum ArmourClass armour_class_from_string(const char* name);
+
+extern struct ArmourBase* g_armour_base;
+extern int g_armour_base_count;
 
 #endif
--- src/obj_armour.c
+++ src/obj_armour.c
@@ -1,137 +1,73 @@
 #include "obj_armour.h"
 
 #include <stdlib.h>
+#include <string.h>
 
+#include "log.h"
 #include "object.h"
 #include "symbol.h"
 
-#define ARMOUR(val, slot, str, agi, in, spi, sta)\
-    { val, slot, str, agi, in, spi, sta, NULL }
+struct ArmourBase* g_armour_base = NULL;
+int g_armour_base_count = 0;
 
-struct Armour* new_armour(int armour_type)
+struct Armour* armour_new(struct ArmourBase* base)
 {
-    struct Armour* armour = (struct Armour*) malloc(sizeof(struct Armour));
+    struct Armour* armour = malloc(sizeof(struct Armour));
 
-    armour->armour_value = armours[armour_type].armour_value;
-    armour->slot = armours[armour_type].slot;
-    armour->strength = armours[armour_type].strength;
-    armour->agility = armours[armour_type].agility;
-    armour->intelligence = armours[armour_type].intelligence;
-    armour->spirit = armours[armour_type].spirit;
-    armour->stamina = armours[armour_type].stamina;
+    armour->armour_value = base->armour_value;
 
-    armour->obj = (struct Object*) malloc(sizeof(struct Object));
-    armour->obj->name = armour_names[armour_type];
-    armour->obj->desc = armour_descs[armour_type];
-    armour->obj->symbol = &symbols[SYM_ARMOUR_OFF + armour_type];
+    armour->obj = malloc(sizeof(struct Object));
+    armour->obj->name = base->name;
+    armour->obj->desc = base->desc;
+
+    switch(base->class)
+    {
+        case ARMOUR_CLASS_CLOTH:
+            armour->obj->symbol = &g_symbol_armour_cloth;
+            break;
+        case ARMOUR_CLASS_LEATHER:
+            armour->obj->symbol = &g_symbol_armour_leather;
+            break;
+        case ARMOUR_CLASS_MAIL:
+            armour->obj->symbol = &g_symbol_armour_mail;
+            break;
+        case ARMOUR_CLASS_PLATE:
+            armour->obj->symbol = &g_symbol_armour_plate;
+            break;
+        case ARMOUR_CLASS_NONE:
+            log_msg(DEBUG, "Armour created with ARMOUR_CLASS_NONE");
+            break;
+    }
+
     armour->obj->objtype_ptr.armour = armour;
     armour->obj->objtype = OBJ_TYPE_ARMOUR;
 
     return armour;
 }
 
-void free_armour(struct Armour* armour)
+void armour_free(struct Armour* armour)
 {
     free(armour->obj);
     free(armour);
 }
 
-//TODO: Refactor to load this info from files rather than be hardcoded
-struct Armour armours[] = 
+struct ArmourBase* armour_base_lookup_by_name(const char* name)
 {
-    ARMOUR(0, EQUIP_SLOT_CHEST, 0, 0, 0, 0, 0),
+    for(int idx = 0; idx < g_armour_base_count; ++idx)
+    {
+        if(strcmp(name, g_armour_base[idx].name) == 0)
+            return &g_armour_base[idx];
+    }
 
-    // Cloth
-    ARMOUR(2, EQUIP_SLOT_HEAD,      0, 1, 7, 3, 6), // Magister's Crown
-    ARMOUR(1, EQUIP_SLOT_SHOULDERS, 0, 2, 4, 4, 4), // Magister's Mantle
-    ARMOUR(3, EQUIP_SLOT_CHEST,     0, 1, 3, 4, 7), // Magister's Robe
-    ARMOUR(1, EQUIP_SLOT_HANDS,     0, 2, 6, 1, 3), // Magister's Gloves
-    ARMOUR(1, EQUIP_SLOT_LEGS,      0, 2, 4, 5, 6), // Magister's Leggings
-    ARMOUR(1, EQUIP_SLOT_FEET,      0, 2, 6, 1, 4), // Magister's Boots
+    return NULL;
+}
 
-    // Leather
-    ARMOUR(4, EQUIP_SLOT_HEAD,      1, 4, 0, 2, 6), // Shadowcraft Cap
-    ARMOUR(2, EQUIP_SLOT_SHOULDERS, 1, 6, 0, 2, 4), // Shadowcraft Spaulders
-    ARMOUR(5, EQUIP_SLOT_CHEST,     4, 3, 0, 2, 7), // Shadowcraft Tunic
-    ARMOUR(2, EQUIP_SLOT_HANDS,     5, 6, 0, 1, 3), // Shadowcraft Gloves
-    ARMOUR(3, EQUIP_SLOT_LEGS,      4, 4, 0, 2, 6), // Shadowcraft Pants
-    ARMOUR(2, EQUIP_SLOT_FEET,      3, 7, 0, 1, 4), // Shadowcraft Boots
-
-    // Mail
-    ARMOUR(6, EQUIP_SLOT_HEAD,      0, 4, 5, 2, 6), // Beaststalker's Cap
-    ARMOUR(4, EQUIP_SLOT_SHOULDERS, 0, 6, 4, 2, 4), // Beaststalker's Spaulders
-    ARMOUR(7, EQUIP_SLOT_CHEST,     0, 3, 3, 2, 7), // Beaststalker's Tunic
-    ARMOUR(4, EQUIP_SLOT_HANDS,     0, 6, 1, 1, 3), // Beaststalker's Gloves
-    ARMOUR(5, EQUIP_SLOT_LEGS,      0, 4, 4, 2, 6), // Beaststalker's Pants
-    ARMOUR(4, EQUIP_SLOT_FEET,      0, 7, 1, 1, 4), // Beaststalker's Boots
-
-    // Plate
-    ARMOUR(8, EQUIP_SLOT_HEAD,      3, 1, 0, 2, 6), // Helm of Valor
-    ARMOUR(6, EQUIP_SLOT_SHOULDERS, 6, 3, 0, 2, 4), // Spaulders of Valor
-    ARMOUR(9, EQUIP_SLOT_CHEST,     4, 1, 0, 2, 7), // Breastplate of Valor
-    ARMOUR(6, EQUIP_SLOT_HANDS,     7, 4, 0, 1, 3), // Gauntlets of Valor
-    ARMOUR(7, EQUIP_SLOT_LEGS,      4, 4, 0, 2, 6), // Legplates of Valor
-    ARMOUR(6, EQUIP_SLOT_FEET,      6, 5, 0, 1, 4), // Boots of Valor
-};
-
-char* armour_names[] =
+enum ArmourClass armour_class_from_string(const char* name)
 {
-    "Unarmoured",
+    if(strcmp(name, "cloth") == 0)   return ARMOUR_CLASS_CLOTH;
+    if(strcmp(name, "leather") == 0) return ARMOUR_CLASS_LEATHER;
+    if(strcmp(name, "mail") == 0)    return ARMOUR_CLASS_MAIL;
+    if(strcmp(name, "plate") == 0)   return ARMOUR_CLASS_PLATE;
 
-    "Magister's Crown",
-    "Magister's Mantle",
-    "Magister's Robe",
-    "Magister's Gloves",
-    "Magister's Leggings",
-    "Magister's Boots",
-
-    "Shadowcraft Cap",
-    "Shadowcraft Spaulders",
-    "Shadowcraft Tunic",
-    "Shadowcraft Gloves",
-    "Shadowcraft Pants",
-    "Shadowcraft Boots",
-
-    "Beaststalker's Cap",
-    "Beaststalker's Spaulders",
-    "Beaststalker's Tunic",
-    "Beaststalker's Gloves",
-    "Beaststalker's Pants",
-    "Beaststalker's Boots",
-
-    "Helm of Valor",
-    "Spaulders of Valor",
-    "Breastplate of Valor",
-    "Gauntlets of Valor",
-    "Legplates of Valor",
-    "Boots of Valor"
-};
-
-char* armour_descs[] =
-{
-    "An unarmoured fighter relies on their dexterity and agility to evade strikes rather than enduring through their opponent's attacks. However, a single failure could be fatal.",
-    "Red and purple regalia favoured by the Mages of the Kirin Tor.",
-    "Red and purple regalia favoured by the Mages of the Kirin Tor.",
-    "Red and purple regalia favoured by the Mages of the Kirin Tor.",
-    "Red and purple regalia favoured by the Mages of the Kirin Tor.",
-    "Red and purple regalia favoured by the Mages of the Kirin Tor.",
-    "Red and purple regalia favoured by the Mages of the Kirin Tor.",
-    "Black leather armour designed for stealth in dark places.",
-    "Black leather armour designed for stealth in dark places.",
-    "Black leather armour designed for stealth in dark places.",
-    "Black leather armour designed for stealth in dark places.",
-    "Black leather armour designed for stealth in dark places.",
-    "Black leather armour designed for stealth in dark places.",
-    "A golden-brown mail worn by the wandering trackers of Kalimdor. This helm is crafted in the likeness of a wolf's head.",
-    "A golden-brown mail worn by the wandering trackers of Kalimdor.",
-    "A golden-brown mail worn by the wandering trackers of Kalimdor.",
-    "A golden-brown mail worn by the wandering trackers of Kalimdor.",
-    "A golden-brown mail worn by the wandering trackers of Kalimdor.",
-    "A golden-brown mail worn by the wandering trackers of Kalimdor.",
-    "Forged from heavy steel plating and decorated with a trim made of gold.",
-    "Forged from heavy steel plating and decorated with a trim made of gold.",
-    "Forged from heavy steel plating and decorated with a trim made of gold.",
-    "Forged from heavy steel plating and decorated with a trim made of gold.",
-    "Forged from heavy steel plating and decorated with a trim made of gold.",
-    "Forged from heavy steel plating and decorated with a trim made of gold."
-};
+    return ARMOUR_CLASS_NONE;
+}
