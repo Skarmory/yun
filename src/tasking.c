5a6a41b62e934130b020139e040b56e15d935292
Song Taezhou <song.taezhou@gmail.com>
2019-08-16 15:53:18 +0100

    Added initial commit of tasking
    The first bit of multithreading in naxx history! The tasker is designed
    to sit there on a separate thread processing "tasks", it will sleep if
    there are no tasks to process.
    
    The tasking system is cooperative, and task execution functions must be
    designed with that philosophy in mind. A task is a wrapper around an
    execution function, its arguments, and a callback function. The task
    args are memcpy'd on task creation so that must also be kept in mind.

---
+++ src/tasking.c
@@ -0,0 +1,263 @@
+#include "tasking.h"
+
+#include "list.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdatomic.h>
+#include <string.h>
+#include <threads.h>
+#include <time.h>
+
+enum TaskerState
+{
+    TASKER_STATE_EXECUTING,
+    TASKER_STATE_STOPPING,
+    TASKER_STATE_STOPPED
+};
+
+struct Tasker
+{
+    atomic_int  state;
+    atomic_bool has_pending_tasks;
+    atomic_bool has_executing_tasks;
+    atomic_bool has_completed_tasks;
+
+    thrd_t      thread;
+    cnd_t       tasks_available_signal;
+    mtx_t       pending_list_lock;
+    mtx_t       complete_list_lock;
+
+    List        task_list;
+    List        pending_list;
+    List        complete_list;
+};
+
+struct Task
+{
+    task_func          func;
+    task_callback_func cb_func;
+    void*              args;
+    atomic_int         status;
+};
+
+/*
+ * This is the "main loop" for the tasker thread.
+ * It waits on a condition variable for tasks to be available.
+ * Tasks are executed in a cooperative manner, so make sure your tasks are cooperative
+ */
+static int _tasker_update(struct Tasker* tasker)
+{
+    while(tasker->state == TASKER_STATE_EXECUTING)
+    {
+        // Add pending to executing
+        if(tasker->has_pending_tasks)
+        {
+            mtx_lock(&tasker->pending_list_lock);
+
+            ListNode *node, *next;
+            list_for_each_safe(&tasker->pending_list, node, next)
+            {
+                list_splice_node(&tasker->pending_list, &tasker->task_list, node);
+            }
+
+            tasker->has_executing_tasks = true;
+            tasker->has_pending_tasks = false;
+
+            mtx_unlock(&tasker->pending_list_lock);
+        }
+
+        // Execute single pass of tasks
+        if(tasker->has_executing_tasks)
+        {
+            ListNode *node, *next;
+            list_for_each_safe(&tasker->task_list, node, next)
+            {
+                struct Task* task = node->data;
+                task->status = task->func(task->args);
+
+                // Task completed, switch it over to the complete list
+                if(task->status == TASK_STATUS_SUCCESS || task->status == TASK_STATUS_FAILED)
+                {
+                    mtx_lock(&tasker->complete_list_lock);
+
+                    list_splice_node(&tasker->task_list, &tasker->complete_list, node);
+                    tasker->has_completed_tasks = true;
+
+                    mtx_unlock(&tasker->complete_list_lock);
+
+                    tasker->has_executing_tasks = (tasker->task_list.count > 0);
+                }
+            }
+        }
+
+        // Need to lock because we are checking for the absence of tasks.
+        // A task could be added just after we check there are none
+        // Therefore resulting in a deadlock
+        mtx_lock(&tasker->pending_list_lock);
+        if(!tasker->has_executing_tasks && !tasker->has_pending_tasks)
+        {
+#ifdef DEBUG
+            log_msg(DEBUG, "No tasks available. Tasker thread going to sleep.");
+#endif
+            cnd_wait(&tasker->tasks_available_signal, &tasker->pending_list_lock);
+        }
+        mtx_unlock(&tasker->pending_list_lock);
+    }
+
+    tasker->state = TASKER_STATE_STOPPED;
+
+    return 1;
+}
+
+struct Tasker* tasker_new(void)
+{
+    struct Tasker* tasker = malloc(sizeof(struct Tasker));
+    tasker->state = TASKER_STATE_EXECUTING;
+    tasker->has_pending_tasks = false;
+    tasker->has_executing_tasks = false;
+    tasker->has_completed_tasks = false;
+
+    cnd_init(&tasker->tasks_available_signal);
+
+    mtx_init(&tasker->pending_list_lock, mtx_plain);
+    mtx_init(&tasker->complete_list_lock, mtx_plain);
+
+    list_init(&tasker->pending_list);
+    list_init(&tasker->task_list);
+    list_init(&tasker->complete_list);
+
+    thrd_create(&tasker->thread, (void*)_tasker_update, tasker);
+
+    return tasker;
+}
+
+void tasker_free(struct Tasker* tasker)
+{
+    tasker->state = TASKER_STATE_STOPPING;
+    while(tasker->state != TASKER_STATE_STOPPED)
+    {
+        // This is necessary to ensure the tasker thread does not deadlock by sleeping on the
+        // condition variable whilst we block waiting for it to join.
+        cnd_signal(&tasker->tasks_available_signal);
+    }
+
+    thrd_join(tasker->thread, NULL);
+    mtx_destroy(&tasker->pending_list_lock);
+    mtx_destroy(&tasker->complete_list_lock);
+    cnd_destroy(&tasker->tasks_available_signal);
+
+    ListNode *node, *next;
+    list_for_each_safe(&tasker->pending_list, node, next)
+    {
+        struct Task* task = node->data;
+        task->status = TASK_STATUS_FAILED;
+        task_free(task);
+        free(node);
+    }
+
+    list_for_each_safe(&tasker->task_list, node, next)
+    {
+        struct Task* task = node->data;
+        task->status = TASK_STATUS_FAILED;
+        task_free(task);
+        free(node);
+    }
+
+    list_for_each_safe(&tasker->complete_list, node, next)
+    {
+        struct Task* task = node->data;
+        task->status = TASK_STATUS_FAILED;
+        task_free(task);
+        free(node);
+    }
+
+    free(tasker);
+}
+
+
+void tasker_integrate(struct Tasker* tasker)
+{
+    if(!tasker->has_completed_tasks)
+        return;
+
+    mtx_lock(&tasker->complete_list_lock);
+
+    ListNode *node, *next;
+    list_for_each_safe(&tasker->complete_list, node, next)
+    {
+        struct Task* task = node->data;
+        task->cb_func(task->args);
+
+        list_rm(&tasker->complete_list, node);
+        task_free(task);
+    }
+
+    mtx_unlock(&tasker->complete_list_lock);
+}
+
+bool tasker_add_task(struct Tasker* tasker, struct Task* task)
+{
+    if(task->status != TASK_STATUS_NOT_STARTED)
+        return false;
+
+    // Add task to pending list
+    mtx_lock(&tasker->pending_list_lock);
+
+    list_add(&tasker->pending_list, task);
+    tasker->has_pending_tasks = true;
+
+    mtx_unlock(&tasker->pending_list_lock);
+
+    cnd_signal(&tasker->tasks_available_signal);
+
+    return true;
+}
+
+bool tasker_has_pending_tasks(struct Tasker* tasker)
+{
+    return tasker->has_pending_tasks;
+}
+
+bool tasker_has_executing_tasks(struct Tasker* tasker)
+{
+    return tasker->has_executing_tasks;
+}
+
+bool tasker_has_completed_tasks(struct Tasker* tasker)
+{
+    return tasker->has_completed_tasks;
+}
+
+struct Task* task_new(task_func func, task_func cb_func, void* args, int size_bytes)
+{
+    struct Task* task = malloc(sizeof(struct Task));
+    task->status = TASK_STATUS_NOT_STARTED;
+    task->func = func;
+    task->cb_func = cb_func;
+    task->args = malloc(size_bytes);
+    memcpy(task->args, args, size_bytes);
+
+    return task;
+}
+
+bool task_free(struct Task* task)
+{
+    if(task->status == TASK_STATUS_EXECUTING)
+        return false;
+
+    free(task->args);
+    free(task);
+
+    return true;
+}
+
+task_func task_get_func(struct Task* task)
+{
+    return task->func;
+}
+
+bool task_is_finished(struct Task* task)
+{
+    return task->status == TASK_STATUS_SUCCESS || task->status == TASK_STATUS_FAILED;
+}
