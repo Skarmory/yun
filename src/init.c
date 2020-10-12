de23ca83e90cffb51f602b9c4b7e5a869503660e
Song Taezhou <song.taezhou@gmail.com>
2020-10-12 11:52:57 +0100

    Updated code to use command manager

--- src/init.c
+++ src/init.c
@@ -1,5 +1,8 @@
 #include "init.h"
 #include "colour.h"
+#include "command_defs.h"
+#include "command_manager.h"
+#include "gameplay.h"
 #include "globals.h"
 #include "log.h"
 #include "mon_attack.h"
@@ -93,6 +96,23 @@ void _uninit_gamedata(void)
     g_attack_methods_count = 0;
 }
 
+static void _init_command_handling(void)
+{
+    g_command_manager = command_manager_new();
+
+    command_manager_register_handler(g_command_manager, COMMAND_TYPE_MOVE, gameplay_command_handler_func);
+    command_manager_register_handler(g_command_manager, COMMAND_TYPE_PASS_TURN, gameplay_command_handler_func);
+    command_manager_register_handler(g_command_manager, COMMAND_TYPE_PICK_UP, gameplay_command_handler_func);
+    command_manager_register_handler(g_command_manager, COMMAND_TYPE_DISPLAY_POSITION, gameplay_command_handler_func);
+    //command_manager_register_handler(COMMAND_TYPE_DISPLAY_CHARACTER_SCREEN,
+    //command_manager_register_handler(COMMAND_TYPE_DISPLAY_INVENTORY,
+}
+
+static void _uninit_command_handling(void)
+{
+    command_manager_free(g_command_manager);
+}
+
 bool init_naxx(void)
 {
     srand(time(NULL));
@@ -109,11 +129,14 @@ bool init_naxx(void)
     if(!_init_gamedata())
         return false;
 
+    _init_command_handling();
+
     return true;
 }
 
 void uninit_naxx(void)
 {
+    _uninit_command_handling();
     _uninit_gamedata();
     tasker_free(g_tasker);
     term_uninit();
--- src/input.c
+++ src/input.c
@@ -1,74 +1,48 @@
 #include "input.h"
 
-#include "map.h"
-#include "map_cell.h"
-#include "map_location.h"
-#include "message.h"
-#include "monster.h"
-#include "mon_attack.h"
-#include "mon_inventory.h"
-#include "movement.h"
-#include "object.h"
-#include "player.h"
+#include "command.h"
+#include "command_manager.h"
+//#include "message.h"
+//#include "mon_inventory.h"
 #include "term.h"
-#include "ui.h"
-#include "ui_inventory.h"
-#include "ui_stats.h"
-#include "util.h"
+//#include "ui.h"
+//#include "ui_inventory.h"
+//#include "ui_stats.h"
+//#include "util.h"
 
 #include <stdbool.h>
 #include <stddef.h>
 
-/**
- * Automatically determine the action of a player given a movement command
- *
- * Will attempt to attack a monster if possible, otherwise will attempt to move
- */
-static bool _do_smart_action(int x, int y)
+static enum CommandType _convert_command(struct Command* command)
 {
-    // This could be null if it's a movement request to an out of bounds x y
-    struct MapCell* cell = map_get_cell_by_world_coord(cmap, x, y);
-    if(!cell) return false;
-
-    struct MapLocation* loc = map_cell_get_location(cell, x, y);
-
-    if(loc->mon == NULL)
-        return move_mon(you->mon, x, y);
-    else
-        return do_attack_mon_mon(you->mon, loc->mon);
-}
-
-/**
- * Try to pick up an object from the floor
- */
-static bool _pick_up_object(void)
-{
-    struct MapCell* cell = map_get_cell_by_world_coord(cmap, you->mon->x, you->mon->y);
-    struct MapLocation* loc = map_cell_get_location(cell, you->mon->x, you->mon->y);
-    List* obj_list = map_cell_get_objects(cell, you->mon->x, you->mon->y);
-
-    if(obj_list->head == NULL)
+    switch(command->cmd_char)
     {
-       display_msg_log("There is nothing here.");
-       return false;
+        case '@':
+            return COMMAND_TYPE_DISPLAY_POSITION;
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'y':
+        case 'u':
+        case 'b':
+        case 'n':
+            return COMMAND_TYPE_MOVE;
+        case '.':
+        case ' ':
+            return COMMAND_TYPE_PASS_TURN;
+        case 'q':
+            return COMMAND_TYPE_SAVE_AND_QUIT;
+            break;
+        case 'c':
+            return COMMAND_TYPE_DISPLAY_CHARACTER_SCREEN;
+        case ',':
+            return COMMAND_TYPE_PICK_UP;
+        case 'i':
+            return COMMAND_TYPE_DISPLAY_INVENTORY;
+        default:
+            return COMMAND_TYPE_UNKNOWN;
     }
-
-    //TODO: Give selection from all objects on floor
-    struct Object* chosen = (struct Object*)obj_list->head->data;
-
-    // Unlink object from Location
-    loc_rm_obj(loc, chosen);
-
-    if(!inventory_add_obj(you->mon->inventory, chosen))
-    {
-        // Failed to add to inventory, relink with Location
-        loc_add_obj(loc, chosen);
-        return false;
-    }
-
-    display_fmsg_log("You picked up a %s.", chosen->name);
-
-    return true;
 }
 
 /**
@@ -79,64 +53,75 @@ static bool _pick_up_object(void)
 void handle_input(void)
 {
     // loop until player has made some valid turn advancing input
-    bool went = false;
+
     do
     {
-        char ch;
-        switch(ch = term_getch()) // get movement
+        struct Command command;
+        command.cmd_char = term_getch();
+        command.type = _convert_command(&command);
+
+        struct CommandResult result = command_manager_handle_command(g_command_manager, &command);
+
+        if(result.end_turn)
         {
-            case '@':
-                display_fmsg_nolog("Current position: %d, %d", you->mon->x, you->mon->y);
-                break;
-            case 'h':
-            case 'j':
-            case 'k':
-            case 'l':
-            case 'y':
-            case 'u':
-            case 'b':
-            case 'n':
-                // set went to true if move success
-                went = _do_smart_action(
-                    you->mon->x + (ch == 'h' || ch == 'y' || ch == 'b' ? -1 : ch == 'l' || ch == 'u' || ch == 'n' ? 1 : 0),
-                    you->mon->y + (ch == 'k' || ch == 'y' || ch == 'u' ? -1 : ch == 'j' || ch == 'b' || ch == 'n' ? 1 : 0)
-                );
-                break;
-            case '.':
-                // Pass turn
-                went = true;
-                break;
-            case 'q':
-                if(prompt_yn("Really quit?"))
-                    do_quit();
-                break;
-            case 'm':
-                display_msg_log("This is a message that is going to be larger than the display area of the message box so this ought be split up into multiple messages");
-                break;
-            case 'M':
-                display_msg_log("This_is_a_message_that_is_going_to_be_larger_than_the_display_area_of_the_message_box_so_this_ought_be_split_up_into_multiple_messages");
-                break;
-            case 'c':
-                went = character_screen_handler();
-                break;
-            case ',':
-                went = _pick_up_object();
-                break;
-            case 'i':
-                went = display_inventory_player();
-                break;
-            case ' ':
-                went = true;
-                break;
-            default:
-                continue;
+            break;
         }
 
-        if(!went)
-        {
-            clear_msgs();
-            flush_msg_buffer();
-            display_main_screen();
-        }
-    } while(!went);
+        //char ch;
+        //switch(ch = term_getch()) // get movement
+        //{
+        //    case '@':
+        //        display_fmsg_nolog("Current position: %d, %d", you->mon->x, you->mon->y);
+        //        break;
+        //    case 'h':
+        //    case 'j':
+        //    case 'k':
+        //    case 'l':
+        //    case 'y':
+        //    case 'u':
+        //    case 'b':
+        //    case 'n':
+        //        // set went to true if move success
+        //        went = _do_smart_action(
+        //            you->mon->x + (ch == 'h' || ch == 'y' || ch == 'b' ? -1 : ch == 'l' || ch == 'u' || ch == 'n' ? 1 : 0),
+        //            you->mon->y + (ch == 'k' || ch == 'y' || ch == 'u' ? -1 : ch == 'j' || ch == 'b' || ch == 'n' ? 1 : 0)
+        //        );
+        //        break;
+        //    case '.':
+        //        // Pass turn
+        //        went = true;
+        //        break;
+        //    case 'q':
+        //        if(prompt_yn("Really quit?"))
+        //            do_quit();
+        //        break;
+        //    case 'm':
+        //        display_msg_log("This is a message that is going to be larger than the display area of the message box so this ought be split up into multiple messages");
+        //        break;
+        //    case 'M':
+        //        display_msg_log("This_is_a_message_that_is_going_to_be_larger_than_the_display_area_of_the_message_box_so_this_ought_be_split_up_into_multiple_messages");
+        //        break;
+        //    case 'c':
+        //        went = character_screen_handler();
+        //        break;
+        //    case ',':
+        //        went = _pick_up_object();
+        //        break;
+        //    case 'i':
+        //        went = display_inventory_player();
+        //        break;
+        //    case ' ':
+        //        went = true;
+        //        break;
+        //    default:
+        //        continue;
+        //}
+
+        //if(!went)
+        //{
+        //    clear_msgs();
+        //    flush_msg_buffer();
+        //    display_main_screen();
+        //}
+    } while(true);
 }
